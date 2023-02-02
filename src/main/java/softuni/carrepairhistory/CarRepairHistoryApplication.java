739c29a60a09088a391682e9601be424dc331040
Ivan Kamberov <ivan_pz86@mail.bg>
2023-02-02 09:41:15 +0200

    create my project

---
+++ src/main/java/softuni/carrepairhistory/CarRepairHistoryApplication.java
@@ -0,0 +1,13 @@
+package softuni.carrepairhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRepairHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRepairHistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/softuni/carrepairhistory/CarRepairHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package softuni.carrepairhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRepairHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
