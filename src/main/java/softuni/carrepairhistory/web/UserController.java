c2f78cd7272b0e0407332dcdbfaf0bcfdaaf926e
Ivan Kamberov <ivan_pz86@mail.bg>
2023-03-23 00:00:11 +0200

    refactor login page

--- src/main/java/softuni/carrepairhistory/web/UserController.java
+++ src/main/java/softuni/carrepairhistory/web/UserController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
