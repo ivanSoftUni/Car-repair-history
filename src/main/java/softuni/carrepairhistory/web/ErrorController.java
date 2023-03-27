27b634f5935f14ae26d3c6bb3bf6d9692449912f
Ivan Kamberov <ivan_pz86@mail.bg>
2023-03-27 22:30:19 +0300

    translate pages

---
+++ src/main/java/softuni/carrepairhistory/web/ErrorController.java
@@ -0,0 +1,15 @@
+package softuni.carrepairhistory.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+
+    @GetMapping("/error")
+    public String getErrorPage() {
+
+        return "404";
+    }
+}
